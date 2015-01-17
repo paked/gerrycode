7220ff1af3991ce66264e77d28d493c247cb407f
Harrison <harrison@theshoebridges.com>
2015-01-17 22:33:12 +1100

    main: add user authentication handlers

--- main.go
+++ main.go
@@ -4,15 +4,15 @@ import (
 	"fmt"
 	// "github.com/freehaha/token-auth"
 	"github.com/gorilla/mux"
-	// "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 	"net/http"
 )
 
 type User struct {
 	Id           bson.ObjectId `bson:"_id"`
-	Username     string
-	PasswordHash string
+	Username     string        `bson:"username",json:"username"`
+	PasswordHash string        `bson:"password_hash",json:"password_hash"`
 	PasswordSalt string
 	Email        string
 }
@@ -21,7 +21,22 @@ type Review struct {
 	From bson.ObjectId `bson:"_id"`
 }
 
+var (
+	session *mgo.Session
+
+	db = "repo-reviews"
+)
+
 func main() {
+	var err error
+
+	session, err = mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+
+	// defer session.Close()
+
 	r := mux.NewRouter()
 	api := r.PathPrefix("/api").Subrouter()
 
@@ -61,6 +76,34 @@ func main() {
 }
 
 func newUserHandler(w http.ResponseWriter, r *http.Request) {
+	username, password := r.FormValue("username"), r.FormValue("password")
+
+	if username == "" || password == "" {
+		fmt.Fprintln(w, "Username or password is not valid")
+		return
+	}
+
+	c := session.DB(db).C("users")
+	var u User
+
+	if err := c.Find(bson.M{"username": username}).One(&u); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if u != (User{}) {
+		fmt.Fprint(w, "That user already exists!")
+		return
+	}
+
+	u = User{Id: bson.NewObjectId(), Username: username, PasswordHash: password}
+
+	if err := c.Insert(u); err != nil {
+		panic(err)
+		return
+	}
+
+	fmt.Fprintf(w, "%v", u)
 
 }
 
@@ -69,6 +112,27 @@ func getUserHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func loginUserHandler(w http.ResponseWriter, r *http.Request) {
+	username, password := r.FormValue("username"), r.FormValue("password")
+
+	if username == "" || password == "" {
+		fmt.Fprintln(w, "your username and password don't have anything in them")
+		return
+	}
+
+	c := session.DB(db).C("users")
+
+	var u User
+
+	if err := c.Find(bson.M{"username": username, "password_hash": password}).One(&u); err != nil {
+		panic(err)
+		return
+	}
+
+	if u == (User{}) {
+		fmt.Fprintln(w, "That user doesnt exist")
+	}
+
+	fmt.Fprintf(w, "yes you are %v", u.Username)
 
 }
 
