9d5e6250f2475308bedbdfc29c11f4e892f3530f
Harrison <harrison@theshoebridges.com>
2015-01-21 21:20:54 +1100

    static/scripts/models: add User model

---
+++ static/scripts/models.js
@@ -0,0 +1,15 @@
+class Repository extends Backbone.Model {
+  initialize () {
+  	console.log("THIS IS A REPOSITORY")
+  
+  	this.defaults = {
+  		host: "Not specified",
+  		user: "Not specified",
+  		repository: "Not specified"
+  	}
+  }
+
+  render() {
+  	if (host == "github.com")
+  }
+}
