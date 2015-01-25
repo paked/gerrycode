c5422e463f840949916470ae7bf4c14fb514116b
Harrison <harrison@theshoebridges.com>
2015-01-25 21:48:58 +1100

    static/: add routing support

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -5,5 +5,19 @@
 
 import MainCtrl from './mainCtrl';
 
-var app = angular.module('Rr', [])
+var app = angular.module('Rr', ['ngRoute'])
           .controller('MainCtrl', MainCtrl);
+
+
+app.config(['$routeProvider', $routeProvider => {
+	$routeProvider.
+		when('/', {
+			templateUrl: 'partials/home.html'
+		}).
+		when('/r/:repository', {
+			templateUrl: 'partials/repository.html'
+		}).
+		otherwise({
+			redirectTo: '/GG.'
+		});
+}]);
