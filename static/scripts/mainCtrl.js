0652dda9bfe6bb12a37d0e78a0a93bee64c59dce
Harrison <harrison@theshoebridges.com>
2015-01-30 08:13:03 +1100

    static/scripts/mainCtrl: redirect to login if no token

--- static/scripts/mainCtrl.js
+++ static/scripts/mainCtrl.js
@@ -1,6 +1,11 @@
 class MainCtrl {
 	constructor() {
 		this.message = "Hello Feddie";
+
+		if (window.token == "" || window.token == undefined) {
+			$(location).attr("href", "#/login");
+			return
+		}
 	}
 
 }
