2112fba4700df394d09f52695ad419d162e7a2d8
Harrison <harrison@theshoebridges.com>
2015-02-28 09:27:27 +1100

    static/*: add routing and "explore" page

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,23 @@
-app = angular.module('revco', []);
+app = angular.module('revco', ["ngRoute"]);
+
+app.config(['$routeProvider',
+	function($routeProvider) {
+		$routeProvider.
+			when('/', {
+				templateUrl: '/partials/home.html',
+				controller: 'MainCtrl'
+			}).
+			when('/view/:project', {
+				templateUrl: '/partials/view_project.html'
+			}).
+			when('/explore', {
+				templateUrl: '/partials/explore.html'
+			}).
+			otherwise({
+				redirectTo: '/'
+			});
+
+	}]);
 
 app.controller('MainCtrl', function($scope) {
 	$scope.message = "PHONETAB EAT PHONETAB";
