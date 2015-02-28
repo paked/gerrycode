0a7059b00964cacb2b3322b14b23e9421221284b
Harrison <harrison@theshoebridges.com>
2015-02-28 23:29:07 +1100

    static/{index, js/main, partials/login}: add login system and routing

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,7 @@
 app = angular.module('revco', ["ngRoute"]);
 
+token = localStorage["token"]
+
 app.config(['$routeProvider',
 	function($routeProvider) {
 		$routeProvider.
@@ -16,6 +18,16 @@ app.config(['$routeProvider',
 			when('/make', {
 				templateUrl: '/partials/make.html'
 			}).
+			when('/auth/:method', {
+				templateUrl: '/partials/login.html',
+				controller: 'AuthCtrl'
+			}).
+			when('/login', {
+				redirectTo: '/auth/login'
+			}).
+			when('/register', {
+				redirectTo: '/auth/register'
+			}).
 			otherwise({
 				redirectTo: '/'
 			});
@@ -24,4 +36,35 @@ app.config(['$routeProvider',
 
 app.controller('MainCtrl', function($scope) {
 	$scope.message = "PHONETAB EAT PHONETAB";
+});
+
+app.controller('AuthCtrl', function($scope, $routeParams, $http) {
+	$scope.method = $routeParams["method"]
+
+	$scope.other = function() {
+		return $scope.method == "login" ? "register" : "login"
+	}
+
+	$scope.go = function() {
+		var url = '/api/user/' + $scope.method + "?username=" + $scope.username + "&password=" + $scope.password + "&email=" + $scope.email;
+		console.log(url);
+		$http.post(url).
+			success(function(data) {
+				if (data.status.error) {
+					$scope.error = data.status.message;
+				}
+
+				localStorage["token"] = data.data.token;
+				token = localStorage["token"];
+			}).
+			error(function(data) {
+				console.log(data);
+			})
+	}
+})
+
+app.controller('HeaderCtrl', function($scope, $location) {
+	if (token == undefined) {
+		$location.path("/login");
+	}
 });
